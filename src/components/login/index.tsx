81c6e7e632435ef67d830dfe8305c7c0dd6d26e5
Ryan Oliveira <97217108+ryanoliveiragit@users.noreply.github.com>
2023-08-18 13:31:35 -0300

    update

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -64,7 +64,7 @@ export const Login = () => {
   });
 
   const handleForm = (data: LoginFormInputs) => {
-    Axios.post("https://backend-ten-olive.vercel.app/login", {
+    Axios.post("https://apipf.jogajuntoinstituto.org/login", {
       email: data.email,
       password: data.password,
     })
--- src/services/getProducts.ts
+++ src/services/getProducts.ts
@@ -7,7 +7,7 @@ export const getProducts = async (
 ) => {
   try {
     const jwt = localStorage.getItem('jwt')
-    const res = await axios.get<ProductsType[]>("https://backend-ten-olive.vercel.app/", { headers: {'Authorization': jwt}});
+    const res = await axios.get<ProductsType[]>("https://apipf.jogajuntoinstituto.org/", { headers: {'Authorization': jwt}});
     setProducts(res.data);
   } catch (error) {
     console.log(error);
